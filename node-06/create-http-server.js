c24688e4eb218a22a503ea34f02bb2a0dacf3776
Aisha Gench <aisha.gench@gmail.com>
2021-05-20 21:58:31 -0500

    use listen for port

--- node-06/create-http-server.js
+++ node-06/create-http-server.js
@@ -1,11 +1,13 @@
 const http = require('http')
 
-const myServer = http.createServer((request,response)=>{
-    console.log(request.url)
-    console.log(request.headers)
-    response.writeHead(200, {'content-type': 'text/html;charset=utf-8'})
-    response.write("<b> Hello World </b> Ü, Ğ, İ, Ş, Ç, Ö from HTTP Server...")
-    response.end()
+const port = 3000
+
+const myServer = http.createServer((request, response)=>{
+  console.log(request.url)
+  console.log(request.headers)
+  response.writeHead(200, {'content-type': 'text/html;charset=utf-8'})
+  //response.write("<b>Hello World </b> Ü, Ğ, İ, Ş, Ç, Ö from HTTP Server....")
+  response.end("<b>Hello World </b> Ü, Ğ, İ, Ş, Ç, Ö from HTTP Server....")
 })
 
-myServer.listen(3000)
+myServer.listen(port)
