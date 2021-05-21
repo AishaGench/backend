603dab59dad98764ae0f14e02a9d243dd6344dbf
Aisha Gench <aisha.gench@gmail.com>
2021-05-20 20:08:25 -0500

    use file-read

---
+++ node-05/file-read.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+fs.readFile('demo.txt',(error,data)=>{
+    if(error){
+        console.log("An error occured during reading the file: ", error)
+    }
+    console.log(data.toString())
+})
