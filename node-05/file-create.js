075a3d98ee484308471ee6979b6fcdacadc821f1
Aisha Gench <aisha.gench@gmail.com>
2021-05-20 20:53:51 -0500

    use writeFile

--- node-05/file-create.js
+++ node-05/file-create.js
@@ -1,9 +1,16 @@
 const fs = require('fs')
-
 // appendFile()     writeFile()
-
+/*
 fs.appendFile('demoNew.txt', "Hello World!!!\n", (err)=>{
   if(err){
     console.log("An error occured during creating file: ", err)
   }
   console.log("Adding data to the file has ended...")
+})
+*/
+fs.writeFile('demoNew.txt', "Hello World!!!\n", (err)=>{
+  if(err){
+    console.log("An error occured during creating file: ", err)
+  }
+  console.log("Adding data to the file has ended...")
+})
