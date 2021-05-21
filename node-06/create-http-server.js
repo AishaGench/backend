7d74bbdffa86bc6fc45131711f590a328aed9c00
Aisha Gench <aisha.gench@gmail.com>
2021-05-20 22:04:48 -0500

    create Nodemon

--- node-06/create-http-server.js
+++ node-06/create-http-server.js
@@ -1,13 +1,11 @@
 const http = require('http')
-
 const port = 3000
-
 const myServer = http.createServer((request, response)=>{
   console.log(request.url)
   console.log(request.headers)
   response.writeHead(200, {'content-type': 'text/html;charset=utf-8'})
   //response.write("<b>Hello World </b> Ü, Ğ, İ, Ş, Ç, Ö from HTTP Server....")
-  response.end("<b>Hello World </b> Ü, Ğ, İ, Ş, Ç, Ö from HTTP Server....")
+  response.end("<b>Hello World </b> Ü, Ğ, İ, Ş, Ç, Ö from HTTP Server from NODEMON....")
 })
 
 myServer.listen(port)
