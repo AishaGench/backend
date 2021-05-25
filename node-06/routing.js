6ff57a5916386a4a8d5d093ccdcaecd8c9e928e6
Aisha Gench <aisha.gench@gmail.com>
2021-05-24 20:07:20 -0500

    create routing js

---
+++ node-06/routing.js
@@ -0,0 +1,17 @@
+// GET  -- POST -- PUT -- DELETE
+
+const http =require('http')
+const port = 3000
+const myServer = http.createServer((request, response)=>{
+    if(request.method === 'GET'){
+        if(request.url === '/'){
+            response.write("<h1></h1>Welcome to HOME page</h1>")
+        }else if(request.url === '/Contact'){
+            response.write("<h1></h1>Welcome to CONTACT page</h1>")
+        }else{
+            response.write("<h1></h1>404 the page not found...</h1>")
+        }
+    }
+})
+
+myServer.listen(port)
