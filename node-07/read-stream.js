1f194f71f6b6e9bbe89fa6dcb350682799239ffa
Aisha Gench <aisha.gench@gmail.com>
2021-05-24 21:00:35 -0500

    read stream

--- node-07/read-stream.js
+++ node-07/read-stream.js
@@ -2,3 +2,11 @@ const fs = require('fs')
 const file ='video.mp4'
 
 const readStream =fs.createReadStream(file)
+
+readStream.on('data',(chunk)=>{
+    console.log('A chunk of data received...');
+})
+
+readStream.on('end',()=>{
+    console.log("Data has been received completely...");
+})
