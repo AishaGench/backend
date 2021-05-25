120dd1ed1a4aef92924e7752efa5f07468aa2052
Aisha Gench <aisha.gench@gmail.com>
2021-05-24 20:52:54 -0500

    create new folder node 07

--- node-06/routing.js
+++ node-06/routing.js
@@ -5,11 +5,11 @@ const port = 3000
 const myServer = http.createServer((request, response)=>{
     if(request.method === 'GET'){
         if(request.url === '/'){
-            response.write("<h1></h1>Welcome to HOME page</h1>")
+            response.write("<h1>Welcome to HOME page</h1>")
         }else if(request.url === '/Contact'){
-            response.write("<h1></h1>Welcome to CONTACT page</h1>")
+            response.write("<h1>Welcome to CONTACT page</h1>")
         }else{
-            response.write("<h1></h1>404 the page not found...</h1>")
+            response.write("<h1>404 the page not found...</h1>")
         }
     }
 })
