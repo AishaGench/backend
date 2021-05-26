22d11aa536e6fde76313d9be787d495993b1cdbb
Aisha Gench <aisha.gench@gmail.com>
2021-05-25 19:44:36 -0500

    create new request for about page

--- node-08/index.js
+++ node-08/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response)=>{
-    response.send('Hello world from ExpressJS..')
-})
+app.get('/', (request, response)=>{response.send('Hello world from ExpressJS..')})
+app.get('/about', (request, response)=>{response.send('Hello ABOUT PAGE from ExpressJS..')})
 
-app.listen(3000)
+app.listen(3000,()=>[console.log('Express Server is running...')])
