97b6b6a00182aaecf2f71c134ba94946ac1c3c7b
Aisha Gench <aisha.gench@gmail.com>
2021-05-25 22:09:29 -0500

    create footer pug and menu pug

--- node-08/pug/index.js
+++ node-08/pug/index.js
@@ -2,5 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//jade = pug
+app.set('view engine', 'pug')
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.render('index.pug', {name:'Demetrius', phone:'(803) 206-1988 x873'}))
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
