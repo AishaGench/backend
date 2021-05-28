7e93f9477e9d0ba521dfe7a86683c6b79fc449d0
Aisha Gench <aisha.gench@gmail.com>
2021-05-27 21:51:45 -0500

    create second public folder

--- node-08/static-folder/index.js
+++ node-08/static-folder/index.js
@@ -4,6 +4,9 @@ const app = express()
 const port = 3000
 app.set('view engine', 'pug')
 app.use(express.static('public'))
+app.use(express.static('public2'))
+
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.render('index'))
+app.get('/index2', (req, res) => res.render('index2'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
