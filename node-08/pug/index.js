d8de9813cd34f6026b27998a47b0f2f7c290142e
Aisha Gench <aisha.gench@gmail.com>
2021-05-27 20:04:44 -0500

    create layout

--- node-08/pug/index.js
+++ node-08/pug/index.js
@@ -9,4 +9,5 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res) => res.render('index.pug', {name:'Demetrius', phone:'(803) 206-1988 x873'}))
 
+app.get('/home',(req, res)=>{res.render('home.pug')})
 app.listen(port, () => console.log(`Example app listening on port port!`))
