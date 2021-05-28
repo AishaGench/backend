1db1ea044c8164d9531c5b56d8b2844797f17729
Aisha Gench <aisha.gench@gmail.com>
2021-05-27 21:35:04 -0500

    create views folder for pug

--- node-08/static-folder/index.js
+++ node-08/static-folder/index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
