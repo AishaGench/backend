3ff03636f69807a1150d62c214a79bbf350f3ce8
Aisha Gench <aisha.gench@gmail.com>
2021-06-01 19:46:18 -0500

    chect on postman CRUD request

--- node-08/static-folder/routing-withexpress/index.js
+++ node-08/static-folder/routing-withexpress/index.js
@@ -4,16 +4,23 @@ const port = 3000
 
 //GET POST PUT DELETE  ---> ALL
 app.get('/', (req, res) => res.send('Hello World from HOME!'))
+// '/' => 'Localhost:3000'
 //app.get('/contact', (req, res) => res.send('Hello World from CONTACT!'))
 
 // ? => optional
 //app.get('/co?nta?ct', (req, res) => res.send('Hello World from CONTACT!'))
 app.get('/co(nta)?ct', (req, res) => res.send('Hello World from CONTACT!'))
 
-// * -> anything
+// * => anything
 //app.get('/con*tact', (req, res) => res.send('Hello World from CONTACT!'))
 
-// + -> repeat character
+// + => repeat character
 app.get('/conta+ct', (req, res) => res.send('Hello World from CONTACT!'))
+// '/contact'  => 'Localhost:3000/contact'
+
+app.get('/about',(request,response) =>response.send('HELLO ABOUT PAGE from GET request'))
+app.post('/about',(request,response) =>response.send('HELLO ABOUT PAGE from POST request'))
+app.put('/about',(request,response) =>response.send('HELLO ABOUT PAGE from PUT request'))
+app.delete('/about',(request,response) =>response.send('HELLO ABOUT PAGE from DELETE request'))
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
