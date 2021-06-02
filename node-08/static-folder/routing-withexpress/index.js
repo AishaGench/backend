ec4dbec35458ea5344ede65c22eb6314e8153063
Aisha Gench <aisha.gench@gmail.com>
2021-06-01 20:29:52 -0500

    create get request

--- node-08/static-folder/routing-withexpress/index.js
+++ node-08/static-folder/routing-withexpress/index.js
@@ -19,12 +19,15 @@ app.get('/conta+ct', (req, res) => res.send('Hello World from CONTACT!'))
 // '/contact'  => 'Localhost:3000/contact'
 
 app.get('/about',(request,response) =>response.send('HELLO ABOUT PAGE from GET request'))
-app.post('/about',(request,response) =>response.send('HELLO ABOUT PAGE from POST request'))
-app.put('/about',(request,response) =>response.send('HELLO ABOUT PAGE from PUT request'))
-app.delete('/about',(request,response) =>response.send('HELLO ABOUT PAGE from DELETE request'))
-app.all('/about',(request,response) =>response.send('HELLO ABOUT PAGE from ALL request'))
+    .post('/about',(request,response) =>response.send('HELLO ABOUT PAGE from POST request'))
+    .put('/about',(request,response) =>response.send('HELLO ABOUT PAGE from PUT request'))
+    .delete('/about',(request,response) =>response.send('HELLO ABOUT PAGE from DELETE request'))
+    .all('/about',(request,response) =>response.send('HELLO ABOUT PAGE from ALL request'))
 //if we put all request at the top another request doesnt work 
 
+//app.get('/user/:id', (req, res)=>{res.send('Hello USERS... ')})
+app.get('/user/:id', (req, res)=>{res.send(req.params)})
+app.get('/user/:id/:jobId', (req, res)=>{res.send('Id= ' + req.params.id + ' Job ID= '+ req.params.jobId)})
 
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
