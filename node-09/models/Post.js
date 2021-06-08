4030570d40f9d8d222a0d3671468f70b6fc8d2c3
Aisha Gench <aisha.gench@gmail.com>
2021-06-07 21:09:59 -0500

    create module

--- node-09/models/Post.js
+++ node-09/models/Post.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const PostSchema = new Schema()
+const PostSchema = new Schema({
+    title: String,
+    createdDate: Date
+})
 
 module.exports = mongoose.model('post', PostSchema)
