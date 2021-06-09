49810408c804b60d7edb3b681efc27aba4452c8e
Aisha Gench <aisha.gench@gmail.com>
2021-06-08 21:02:30 -0500

    create get method

--- node-09/models/Post.js
+++ node-09/models/Post.js
@@ -1,10 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
 const PostSchema = new Schema({
-    title: String,
-    subTitle: {type: String, required:true, unique:true},
-    createdDate: {type:Date, default:Date.now},
-    isActive: {type:Boolean, default:true},
-    comments: [{message:String}],
-    meta: {votes: Number, favs: Number}
-  })
-  
-  module.exports = mongoose.model('post', PostSchema) 
+  title: String,
+  subTitle: {type: String, required:true, unique:true},
+  createdDate: {type:Date, default:Date.now},
+  isActive: {type:Boolean, default:true},
+  comments: [{message:String}],
+  meta: {votes: Number, favs: Number}
+})
+
+module.exports = mongoose.model('post', PostSchema) 
--- node-09/routes/post.routes.js
+++ node-09/routes/post.routes.js
@@ -4,23 +4,45 @@ const PostModel = require('../models/Post')
 
 // Created by node-express-post snippet
 router.post('/', function (req, res) {
-    const myPostModel = new PostModel({
-      title: "My New Title 11",
-      subTitle:"My new subTitle",
+  const myPostModel = new PostModel({
+    title: "My New Title 11",
+    subTitle: "My new subtitle 2",
     //createdDate: Date.now(),
-      isActive: false,
-      meta:{votes: 85, favs: 10},
-      comments: [
-        {message:"Good job!!!"},
-        {message:"I didn't like..."}
-      ]
-  
-    })
-    myPostModel.save((err, data)=>{
-      if(err) res.json(err)
-      res.json(data)
-    })
-    //res.send('POST request to the homepage')
+    isActive: false,
+    meta:{votes:54, favs: 45},
+    comments: [
+      {message:"Good job!!!"},
+      {message:"It's fair..."}
+    ]
+
   })
-  
-  module.exports = router
+  myPostModel.save((error, data)=>{
+    if(error) res.json(error)
+    res.json(data)
+  })
+})
+
+// Find() --> To get all data from the collection
+/*
+router.get('/', (req, res) => {
+  PostModel.find((resultData,err)=>{
+    if(err) res.json(err)
+    res.json(resultData)})
+})
+*/
+
+router.get('/', (req, res) => {
+  PostModel.find()
+  .then((resultData)=>{res.json(resultData)})
+  .catch((err)=>{res.json(err)})
+})
+
+// Filter the results in find()
+router.get('/search', (req, res) => {
+    PostModel.find({isActive:false, title:"My New Title 6"}, 'title subTitle')
+    .then((resultData)=>{res.json(resultData)})
+    .catch((err)=>{res.json(err)})
+  })
+
+
+module.exports = router
