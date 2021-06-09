9c9f3b2ff1aceac747d203371b1c655ec85dd698
Aisha Gench <aisha.gench@gmail.com>
2021-06-08 19:40:28 -0500

    create new post

--- node-09/models/Post.js
+++ node-09/models/Post.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema
 
 const PostSchema = new Schema({
     title: String,
-    createdDate: Date
+    createdDate: Date,
+    isActive: Boolean,
+    comments:[{message:String}],
+    meta:{vaotes:Number,favs:Number}
 })
 
 module.exports = mongoose.model('post', PostSchema)
--- node-09/routes/post.routes.js
+++ node-09/routes/post.routes.js
@@ -2,18 +2,24 @@ const express = require('express')
 const router = express.Router()
 const PostModel = require('../models/Post')
 
-//created by node-express-post snoppet
+// Created by node-express-post snippet
 router.post('/', function (req, res) {
     const myPostModel = new PostModel({
-        title:"My New Title 2",
-        createDate: Date.now()
+      title: "My New Title 6",
+      createdDate: Date.now(),
+      isActive: false,
+      meta:{votes: 95, favs: 100},
+      comments: [
+        {message:"Good job!!!"},
+        {message:"I didn't like..."}
+      ]
+  
     })
     myPostModel.save((error, data)=>{
-        if(error)res.json(error)
-        res.json(data)
+      if(error) res.json(error)
+      res.json(data)
     })
-  //res.send('POST request to the homepage')
-})
-
-
-module.exports = router
+    //res.send('POST request to the homepage')
+  })
+  
+  module.exports = router
