1aa1b6ffcac2a05ff7cc50856b366817c934cc5d
Aisha Gench <aisha.gench@gmail.com>
2021-06-08 20:06:41 -0500

    use unique key-word

--- node-09/models/Post.js
+++ node-09/models/Post.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
 const PostSchema = new Schema({
     title: String,
-    createdDate: Date,
-    isActive: Boolean,
-    comments:[{message:String}],
-    meta:{vaotes:Number,favs:Number}
-})
-
-module.exports = mongoose.model('post', PostSchema)
+    subTitle: {type: String, required:true, unique:true},
+    createdDate: {type:Date, default:Date.now},
+    isActive: {type:Boolean, default:true},
+    comments: [{message:String}],
+    meta: {votes: Number, favs: Number}
+  })
+  
+  module.exports = mongoose.model('post', PostSchema) 
--- node-09/routes/post.routes.js
+++ node-09/routes/post.routes.js
@@ -5,18 +5,19 @@ const PostModel = require('../models/Post')
 // Created by node-express-post snippet
 router.post('/', function (req, res) {
     const myPostModel = new PostModel({
-      title: "My New Title 6",
-      createdDate: Date.now(),
+      title: "My New Title 11",
+      subTitle:"My new subTitle",
+    //createdDate: Date.now(),
       isActive: false,
-      meta:{votes: 95, favs: 100},
+      meta:{votes: 85, favs: 10},
       comments: [
         {message:"Good job!!!"},
         {message:"I didn't like..."}
       ]
   
     })
-    myPostModel.save((error, data)=>{
-      if(error) res.json(error)
+    myPostModel.save((err, data)=>{
+      if(err) res.json(err)
       res.json(data)
     })
     //res.send('POST request to the homepage')
