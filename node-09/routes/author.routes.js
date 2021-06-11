a93c0a41905683dc40abab136cb445cf9af4abaf
Aisha Gench <aisha.gench@gmail.com>
2021-06-10 22:12:23 -0500

    create aggregate method

--- node-09/routes/author.routes.js
+++ node-09/routes/author.routes.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const router = express.Router()
 
+const AuthorModel = require('../models/Author')
+
 // Snippet -> node-express-get
-app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+router.get('/', (req, res) => {
+  res.send('GET request to the AUTHOR PAGE')
 })
 
-// Snippet -> node-express-post
-app.post('/', function (req, res) {
-    const newAuthor = new AuthorModel(req.body)
-    newAuthor.save()
-    .then(result =>res.json(result))
-    .catch(err=>res.json(err))
+// POST, snippet->  node-express-post
+router.post('/', function (req, res) {
+  const newAuthor = new AuthorModel(req.body)
+  newAuthor.save()
+  .then(result=>res.json(result))
+  .catch(err => res.json(err))
 })
-module.exports = router
- 
+
+
+router.get('/aggregate',(req, res)=>{
+    AuthorModel.aggregate([
+      /*{
+        $match: {isDelete: false}
+      },
+      {
+        $group: {_id:"$category", totalCount:{$sum:1}}
+      },*/
+      {
+        $project:{name:true, score:true}
+      }
+    ])
+    .then(data=>res.json(data))
+    .catch(error=>res.json(error))
+  })
+  
+  module.exports = router
