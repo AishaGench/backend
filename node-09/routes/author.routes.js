9298e99bb1fc881d45b0708f490e4194ccee87c0
Aisha Gench <aisha.gench@gmail.com>
2021-06-10 21:48:12 -0500

    create post method

--- node-09/routes/author.routes.js
+++ node-09/routes/author.routes.js
@@ -6,5 +6,12 @@ app.get('/', (req, res) => {
   res.send('GET request to the homepage')
 })
 
+// Snippet -> node-express-post
+app.post('/', function (req, res) {
+    const newAuthor = new AuthorModel(req.body)
+    newAuthor.save()
+    .then(result =>res.json(result))
+    .catch(err=>res.json(err))
+})
 module.exports = router
  
